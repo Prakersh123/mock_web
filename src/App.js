101b03650d1df7a0ad024b2364c38184e50686cc
arya <prakersh009@gmail.com>
2021-07-04 16:47:41 +0530

    image_added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Contain from './Contain';
+import Name11 from './Name11.png'
+// import ''
 function App() {
   return (
     <>
     <div className="top_box">
-        <img src="http://recsonbhadra.ac.in/assets/img/Name11.png" alt="no" />
+        <img src={Name11} alt="no" />
         <h6 style={{color:'white'}}>An AICTE Approved Government Engineering College, Affiliated to AKTU Lucknow</h6>
     </div>
     <div className="top_box_two">
